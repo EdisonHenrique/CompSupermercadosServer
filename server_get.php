4a265f992bc921fa56bb12006f647da2608242ca
calmoon <jvitor210@hotmail.com>
2021-11-09 15:59:19 -0300

    Refeito o arq. de gets com a nova base lógica

--- server_get.php
+++ server_get.php
@@ -1,80 +1,71 @@
 <?php
     
-    $response = array();
+	require_once "util.php";
+		
+	function require_id() {
+		check_request_params("GET", ["id"]);
+		return $_GET["id"];
+	}
 
-    if (!empty($_GET)) {
-        $selectCommand = "SELECT " . $_GET["select"] . "FROM " . $_GET["from"];
+	// ------------------------------------------------- //
 
-        if (!empty($_GET["where"])) {
-            $selectCommand .= "WHERE " . $_GET["where"];
-        }
+	check_request_params("GET", ["queryType"]);
+	
+	// Determine SQL query
+	switch( $_GET["queryType"] ) {
 
-        $con = pg_connect(getenv("DATABASE_URL"));
-        $result = pg_query($con, $selectCommand);
-        //Codigo copiado do professor
-        if (!empty($result)) {
-            if (pg_num_rows($result) > 0) {
-     
-                // Se o produto existe, os dados de detalhe do produto 
-                // sao adicionados no array de resposta.
-                $result = pg_fetch_array($result);
-     
-                //Foreach criado para dinamização da inserção das chaves
-                //e valores dentro do array de consulta que vai ser retornado
-                $consult = array();
-                foreach ($result as $key => $value) {
-                    $consult[$key] = $value;
-                }
-                
-                // Caso o produto exista no BD, o cliente 
-                // recebe a chave "success" com valor 1.
-                $response["success"] = 1;
-     
-                $response["product"] = array();
-     
-                // Converte a resposta para o formato JSON.
-                array_push($response["consult"], $consult);
-                
-                // Fecha a conexao com o BD
-                pg_close($con);
-     
-                // Converte a resposta para o formato JSON.
-                echo json_encode($response);
-            } else {
-                // Caso o produto nao exista no BD, o cliente 
-                // recebe a chave "success" com valor 0. A chave "message" indica o 
-                // motivo da falha.
-                $response["success"] = 0;
-                $response["message"] = "Produto não encontrado";
-                
-                // Fecha a conexao com o BD
-                pg_close($con);
-     
-                // Converte a resposta para o formato JSON.
-                echo json_encode($response);
-            }
-        } else {
-            // Caso o produto nao exista no BD, o cliente 
-            // recebe a chave "success" com valor 0. A chave "message" indica o 
-            // motivo da falha.
-            $response["success"] = 0;
-            $response["message"] = "Produto não encontrado";
-     
-            // Fecha a conexao com o BD
-            pg_close($con);
-     
-            // Converte a resposta para o formato JSON.
-            echo json_encode($response);
-        }
-    } else {
-        // Se a requisicao foi feita incorretamente, ou seja, os parametros 
-        // nao foram enviados corretamente para o servidor, o cliente 
-        // recebe a chave "success" com valor 0. A chave "message" indica o 
-        // motivo da falha.
-        $response["success"] = 0;
-        $response["message"] = "Campo requerido não preenchido";
-     
-        // Converte a resposta para o formato JSON.
-        echo json_encode($response);
-    }
+		default:
+			exit_with_error_response("Invalid query name");
+			
+		case "productCatalog":
+			$query = "
+				SELECT produto.nome
+					 , preco_atual
+					 , imagem 
+				FROM item
+				INNER JOIN produto ON item.id_produto = produto.id
+				INNER JOIN supermercado ON item.id_supermercado = supermercado.id
+			";
+			break;
+		
+		case "userInfo":
+			$id = require_id();
+			$query = "
+				SELECT nome
+					 , data_nascimento
+					 , email 
+				FROM usuario
+				WHERE id = $id
+			";
+			break;
+
+	}
+
+	// Attempt server connection
+	$conn = pg_connect(getenv("DATABASE_URL"));
+	if (!$conn){
+		exit_with_error_response("Server connection failed");
+	}
+	
+	// Run SQL query
+	$result = pg_query($conn, $query);
+	
+	// Close server connection 
+	pg_close($conn);
+
+	// Echo query result/error JSON
+	if (!$result) {
+		$resultError = pg_result_error($result);
+		exit_with_error_response("Query error: $resultError");
+	}
+	else {
+		$resultData = pg_fetch_assoc($result);
+		if ($resultData) {
+			output_json_response(1, $resultData);
+		} 
+		else {
+			output_json_response(0, "Query returned no results");
+		}
+	}
+	
 ?>
