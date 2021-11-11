945e8e3f6790eb40c954875b6f7ef151ad5f833a
EdisonHenrique <edinhobatalha1@gmail.com>
2021-11-11 09:38:58 -0300

    Evolução do arq post para insert

---
+++ server_insert.php
@@ -0,0 +1,67 @@
+<?php
+
+    require_once "util.php";
+
+    function makeInsert() {
+        $insertComand = "INSERT INTO " . trim($_POST["table"]) . "(";
+        $insertValues = "VALUES (";
+        foreach ($_POST as $key => $value) {
+            if ($key != "table") {
+                $value = trim($value);
+                $insertComand .= $key . ", ";
+                $insertValues .= "'$value', ";
+            }
+        } 
+        $insertComand = substr_replace($insertComand,") ", strlen($insertComand) -2);
+        $insertValues = substr_replace($insertValues,")", strlen($insertValues) -2);
+        $query = $insertComand . $insertValues;
+        //if (!empty($dados[0][0])) {
+        //    $insertComand = $insertComand . "WERE id = " . $dados[0][0];
+        //}
+        return $query;
+    }
+
+    // Create the parameters array
+    $parameters = array();
+    foreach ($_POST as $key => $value) {
+        array_push($parameters, $key);
+    }
+    
+    check_superglobal_params("POST", $parameters);
+
+    $query = makeInsert();
+
+    // Attempt server connection
+    $conn = pg_connect(getenv("DATABASE_URL"));
+    if (!$conn){
+		exit_with_error_response("Server connection failed");
+	}
+    
+    // Run SQL query
+	$result = pg_query($conn, $query);
+
+    // Close server connection 
+	pg_close($conn);
+
+    if (!$result) {
+		$resultError = pg_result_error($result);
+		exit_with_error_response("Query error: $resultError");
+	}
+	else {
+		output_json_response(1, $_POST["table"] . "criado com suceesso.");
+	}
+
+    /* Tem um problema aqui, essa função precisa filtrar
+    caso aja a superglobal $_FILES, caso a superglobal 
+    não exista o codigo deve ser executado normalmente,
+    caso ela exista e passe pelo isset ela deve ser adicionada
+    ao array de dados, caso não passe pelo isset o codigo
+    deve parar e retornar que um item está faltando
+    if (!empty($_FILES)) {
+        //Codigo para imagem do professor
+        $imageFileType = strtolower(pathinfo(basename($_FILES["img"]["name"]),PATHINFO_EXTENSION));
+        $image_base64 = base64_encode(file_get_contents($_FILES['img']['tmp_name']) );
+        $img = 'data:image/'.$imageFileType.';base64,'.$image_base64;
+        $dados = array_merge($dados, array($count => array("img", $img)));
+    }*/
+?>
