e5d477630ea17cabd6b3831e46ef191dc1331676
calmoon <jvitor210@hotmail.com>
2021-11-22 19:21:42 -0300

    correção 2 do teste

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 
-    $consult_bd_url = $_SERVER["SERVER_ADDR"] . "/server_select.php?queryType=itemInfo&id=1";
+    $consult_bd_url = $_SERVER["SERVER_NAME"] . "/server_select.php?queryType=itemInfo&id=1";
 
     $curl = curl_init($consult_bd_url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -12,6 +12,7 @@
     $object = json_decode($data);
 
     echo var_dump($object);
-    echo $_SERVER["SERVER_ADDR"];
+    echo var_dump($consult_bd_url);
+    echo $_SERVER["SERVER_NAME"];
 
 ?>
