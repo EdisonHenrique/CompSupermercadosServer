6f60775d3af548f4c5404039fa8ba3901a4d35b7
calmoon <34622083+calmoon@users.noreply.github.com>
2022-04-14 10:11:43 -0300

    Revert, troca token

--- bluesoftApi.php
+++ bluesoftApi.php
@@ -24,22 +24,6 @@
         else return false;
     }
 
-    function get_specified_product_id($barcode) {
-        $query = "
-            SELECT produto.id
-            FROM produto
-            WHERE cod_barras = '$barcode'
-        ";
-
-        $resultData = run_query($query);
-
-        if ($resultData) {
-            return $resultData[0]["id"];
-        } 
-        
-        else return false;
-    }
-
     check_superglobal_params("POST", ["barcode", "supermarketId"]);
 
     $barcode = $_POST["barcode"];
@@ -47,8 +31,69 @@
 
     // Se a tentativa de obter o item especificado falhar
     if ( !get_specified_item_id($barcode, $supermarketId) ) {
+        /*
+            INICIO DO REQUEST À API BLUESOFT COSMOS
+        */
+        $url = "https://api.cosmos.bluesoft.com.br/gtins/$barcode.json";
+        $agent = "Cosmos-API-Request";
+        $headers = array(
+            "Content-Type: application/json",
+            "X-Cosmos-Token: bJ2x_WUhAmLnawr0dI50Mw"
+        );
+        
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_USERAGENT, $agent);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_FAILONERROR, true);
+        
+        $data = curl_exec($curl);
+        
+        if ($data === false || $data == NULL) {
+            $resultError = curl_error($curl);
+            throw_exception_response("Bluesoft Cosmos query error: $resultError");
+        }
+        
+        curl_close($curl);
+        /*  
+        FIM DO REQUEST À API
+        */
+        
+        $object = json_decode($data, true); // Array assoc. de dados retornados pela API  
+        
+        // Obtenção do nome do produto
+        $productName = var_or_default($object['description']);
+        
+        // Obtenção da imagem
+        $productImageUrl = var_or_default($object['thumbnail']);
+        if ($productImageUrl == "") {
+            $productImageUrl = var_or_default($object['brand']['picture']); // se não tem foto, pega foto da marca
+        }
+        if ($productImageUrl == "" or !filter_var($productImageUrl, FILTER_VALIDATE_URL)) {
+            $productImageUrl = "https://comp-supermercados.s3.sa-east-1.amazonaws.com/default.jpeg"; // se não tem foto da marca, coloca default
+        }
+        
+        // Obtenção do preço médio
+        $itemAvgPrice = var_or_default($object['avg_price'], 0); // se avg_price vazio, seta como 0
 
-        // Verificando se o produto já existe
+
+        // Criação de novo produto no BD.
+        // O valor '1' é o tipo_produto padrão, por enquanto
+        // TODO: criar lógica para definir o tipo_produto a partir dos dados da API
+        $query = "
+            INSERT INTO produto (cod_barras, nome, imagem_url, id_tipo_produto)
+            VALUES ( 
+                '$barcode', 
+                '$productName', 
+                '$productImageUrl', 
+                1
+            )
+        ";
+        $possibleInsertError = run_query($query, $returnErrorInsteadOfExiting=true);
+
+
+        // Obtenção do id do produto em questão
         $query = "
             SELECT id 
             FROM produto
@@ -56,118 +101,29 @@
         ";
         $resultData = run_query($query);
 
-        // Só consulta BLuesoft se produto não está no nosso banco
-        // TODO: polir isso
-        if (!$resultData) {
-            /*
-            INICIO DO REQUEST À API BLUESOFT COSMOS
-            */
-            $url = "https://api.cosmos.bluesoft.com.br/gtins/$barcode.json";
-            $agent = "Cosmos-API-Request";
-            $headers = array(
-                "Content-Type: application/json",
-                "X-Cosmos-Token: uCT7hFrsRUDdIA4JD9U26A"
-            );
-            
-            $curl = curl_init($url);
-            curl_setopt($curl, CURLOPT_USERAGENT, $agent);
-            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_FAILONERROR, true);
-            
-            $data = curl_exec($curl);
-            
-            if ($data === false || $data == NULL) {
-                $resultError = curl_error($curl);
-                throw_exception_response("Bluesoft Cosmos query error: $resultError");
-            }
-            
-            curl_close($curl);
-            /*  
-            FIM DO REQUEST À API
-            */
-            
-            $object = json_decode($data, true); // Array assoc. de dados retornados pela API  
-            
-            // Obtenção do nome do produto
-            $productName = var_or_default($object['description']);
-            
-            // Obtenção da imagem
-            $productImageUrl = var_or_default($object['thumbnail']);
-            if ($productImageUrl == "") {
-                $productImageUrl = var_or_default($object['brand']['picture']); // se não tem foto, pega foto da marca
-            }
-            if ($productImageUrl == "" or !filter_var($productImageUrl, FILTER_VALIDATE_URL)) {
-                $productImageUrl = "https://comp-supermercados.s3.sa-east-1.amazonaws.com/default.jpeg"; // se não tem foto da marca, coloca default
-            }
-            
-            // Obtenção do preço médio
-            $itemAvgPrice = var_or_default($object['avg_price'], 0); // se avg_price vazio, seta como 0
+        $productId = $resultData[0]["id"];
 
-
-            // Criação de novo produto no BD.
-            // O valor '1' é o tipo_produto padrão, por enquanto
-            // TODO: criar lógica para definir o tipo_produto a partir dos dados da API
-            $query = "
-                INSERT INTO produto (cod_barras, nome, imagem_url, id_tipo_produto)
-                VALUES ( 
-                    '$barcode', 
-                    '$productName', 
-                    '$productImageUrl', 
-                    1
-                )
-            ";
-            $possibleInsertError = run_query($query, $returnErrorInsteadOfExiting=true);
-
-            // Obtenção do id do produto em questão
-            $query = "
-                SELECT id 
-                FROM produto
-                WHERE cod_barras = '$barcode'
-            ";
-            $resultData = run_query($query);
-
-            $productId = $resultData[0]["id"];
-
-            if ($productId == "" or $productId == NULL) { // $possibleInsertError confirmado
-                throw_exception_response($possibleInsertError);
-            }
-
-            // Criação do novo item de supermercado no BD
-            $query = "
-                INSERT INTO item (preco_atual, data_alter_preco, id_supermercado, id_produto)
-                VALUES (
-                    $itemAvgPrice,
-                    now(),
-                    $supermarketId,
-                    $productId
-                )
-            ";
-            run_query($query);
+        if ($productId == "" or $productId == NULL) { // $possibleInsertError confirmado
+            throw_exception_response($possibleInsertError);
         }
+    
 
-        // Se já tem o produto, só aproveita ele com rpeço padrão 1
-        else {
-            $productId = $resultData[0]["id"];
-            
-            // Criação do novo item de supermercado no BD
-            $query = "
-                INSERT INTO item (preco_atual, data_alter_preco, id_supermercado, id_produto)
-                VALUES (
-                    1,
-                    now(),
-                    $supermarketId,
-                    $productId
-                )
-            ";
-        }
+        // Criação do novo item de supermercado no BD
+        $query = "
+            INSERT INTO item (preco_atual, data_alter_preco, id_supermercado, id_produto)
+            VALUES (
+                $itemAvgPrice,
+                now(),
+                $supermarketId,
+                $productId
+            )
+        ";
+        run_query($query);
+        
 
         // Ao fim desse processo, é para termos criado um novo produto e/ou item,
         // então buscamos por ele para que seja retornado no response.
-        get_specified_item_id($barcode, $supermarketId);
-
-                
+        get_specified_item_id($barcode, $supermarketId);        
     }
 
     ?>
